7624c279fd49a311948322b55f8b6866e958c640
loehrfelix <loehrfelix@37215579-ecf6-e6d3-c2ff-48063d7f5a7c>
2011-05-04 10:23:56 +0000

    Bitte ab sofort nurnoch hier weiterarbeiten. Bitte die eigenen Files updaten.

---
+++ Netzwerktamagotchi/src/GAME/Food.java
@@ -0,0 +1,128 @@
+package GAME;
+
+
+public class Food implements Comparable<Food> {
+
+    private double startValue = 0;
+    private final double lowLimit = -10;
+    private long MHD = 0;
+    private long TTL = 0;
+    private long creation = 0;
+    private int fieldX = 0;
+    private int fieldY = 0;
+
+    public int getPosX() {
+            return getFieldX()*50;
+    }
+
+    public int getPosY() {
+            return getFieldY()*50;
+    }
+
+    /**
+     * @return the fieldX
+     */
+    public int getFieldX() {
+        return fieldX;
+    }
+
+    /**
+     * @return the fieldY
+     */
+    public int getFieldY() {
+        return fieldY;
+    }
+	
+    private int index = 0;
+
+    public int getIndex() {
+            return index;
+    }
+
+    public void setIndex(int index) {
+            this.index = index;
+    }
+
+    public int getMHDinSec(){
+        return (int) MHD/1000;
+    }
+
+
+    public Food(int Value) {
+             this.startValue = Value;
+    }
+
+//	TODO nur f�r testzwecke
+    public Food(int X, int Y) {
+            this.fieldX = X;
+            this.fieldY = Y;
+    }
+
+    public Food(int Value, long TTL, long MHD, int posX, int posY){
+        creation = System.currentTimeMillis();
+        this.startValue = Value;
+        this.TTL = TTL;
+        this.MHD = MHD;
+        this.fieldX = posX;
+        this.fieldY = posY;
+    }
+
+    @Override
+    public String toString() {
+            return "Value = " + this.startValue;
+    }
+
+    public int getFoodValue(){
+        if (System.currentTimeMillis() >= creation + MHD ){
+            if (System.currentTimeMillis() >= creation + 2*MHD){
+                return (int)lowLimit;
+            } else {
+                double deterioation = lowLimit / MHD;
+                return (int)((System.currentTimeMillis() - creation - MHD) * deterioation);
+            }
+        } else {
+
+            double deterioation = (startValue) / MHD;
+            return (int)(startValue - (System.currentTimeMillis() - creation) * deterioation);
+        }
+    }
+
+    public boolean checkState(){
+            if (System.currentTimeMillis() > TTL ){
+                    return false;
+            } else {
+                    return true;
+            }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+            if ( obj == null || this.getClass() != obj.getClass() ) {
+                    return false;
+            } else {
+                    Food tmp = (Food) obj;
+                    return getFieldX() == tmp.getFieldX() && getFieldY() == tmp.getFieldY();
+            }
+    }
+
+    @Override
+    public int hashCode() {
+//		int tmpPosX = posX; 
+//		for ( int i= 0; i < Math.floor(Math.log10(posY))+1 ; i++ ){
+//			tmpPosX = tmpPosX * 10;
+//		}
+//		return tmpPosX + posY;
+            return getFieldX()*100000 + getFieldY();
+    }
+
+@Override
+    public int compareTo(Food o) {
+            // TODO Auto-generated method stub
+            int comparison = getFieldX() - o.getFieldX();
+            if(comparison != 0){
+                    return comparison;
+            }
+            return getFieldY() - o.getFieldY();
+    }
+
+}
---
+++ Netzwerktamagotchi/src/GUI/Main.fx
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * Testbeitrag von Steffen ;)
+ * von Sebastian
+ * blablabla
+ */
+
+package GUI;
+
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+
+/**
+ * @author sschmiech
+ */
+
+Stage {
+    title: "Application title"
+    scene: Scene {
+        width: 250
+        height: 80
+        content: [
+            Text {
+                font: Font {
+                    size: 16
+                }
+                x: 10
+                y: 30
+                content: "Application content"
+            }
+        ]
+    }
+}
